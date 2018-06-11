14d5320ea8b0cc8e1690aec89251e321560ae5f2
Mohammed Elkady <melkadi@instabug.com>
2018-06-11 20:51:15 +0200

    add seeds add the required seeds for location groups and target group s

--- project/db/seeds.rb
+++ project/db/seeds.rb
@@ -29,6 +29,25 @@ LOCATIONS = [
   { name: "Seattle" }
 ].freeze
 
+def create_tree(root, depth)
+  return root if depth.zero?
+
+  children_count = Faker::Number.between 1, 3
+
+  children = (1..children_count).map do
+    root.children.create!(target_attrs(root.panel_provider_id))
+  end
+
+  children.each { |child| create_tree(child, depth - 1) }
+end
+
+def target_attrs(provider_id)
+  { panel_provider_id: provider_id,
+    name: Faker::Lorem.word,
+    external_id: SecureRandom.uuid,
+    secret_code: SecureRandom.hex(64) }
+end
+
 PANEL_PROVIDERS_CODES.each { |panel_provider_code| PanelProvider.create!(code: panel_provider_code) }
 
 COUNTRIES.each do |country|
@@ -45,3 +64,20 @@ LOCATIONS.each do |location|
     secret_code: SecureRandom.hex(64)
   )
 end
+
+# create target & location groups
+root_target_groups = []
+us_company_id = Country.find_by!(code: 'US').id # as all locations in us
+
+PanelProvider.all.ids.cycle do |provider_id|
+  LocationGroup.create!(
+    panel_provider_id: provider_id,
+    country_id: us_company_id,
+    name: Faker::Lorem.word)
+
+  root_target_groups << TargetGroup.create(target_attrs(provider_id))
+
+  break if root_target_groups.size == 4
+end
+
+root_target_groups.each { |trgt| create_tree(trgt, 3) }
