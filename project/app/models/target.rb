235e87bee68c709c6e59c64e0a38a2e0f6c2cb61
Mohammed Elkady <melkadi@instabug.com>
2018-06-13 16:25:31 +0200

    Target model implment Model Target ( non active record model) to contain and validate the params for target eval and specs for this model

--- project/app/models/target.rb
+++ project/app/models/target.rb
@@ -1,3 +1,52 @@
+# frozen_string_literal: true
 
 class Target
+  include ActiveModel::Validations
+  validate :country_existence, :target_group_existence, :locations_existence, :target_country_relation
+
+  def initialize(country_code, target_group_id, locations)
+    @country_code = country_code
+    @target_group_id = target_group_id
+    @locations = locations
+  end
+
+  def country_existence
+    return if Country.exists?(code: @country_code)
+    errors.add(:country, "No country with code #{@country_code}")
+  end
+
+  def target_group_existence
+    return if TargetGroup.exists?(@target_group_id)
+    errors.add(:target_group, "No country with code #{@country_code}")
+  end
+
+  def target_country_relation
+    return unless Country.exists?(code: @country_code)
+    return if Country.find_by_code(@country_code).target_groups.exists?(@target_group_id)
+
+    errors.add(:target_country_relation, 'Target not available in this country')
+  end
+
+  # TODO: validate if the location linked to the company or not
+  def locations_existence
+    not_exists_ids = @locations.reject do |loc|
+      Location.exists?(loc[:id])
+    end
+
+    return if not_exists_ids.blank?
+
+    errors.add(:locations, "No locations with ids #{not_exists_ids}")
+  end
+
+  def eval
+    panel_provider = Country.find_by_code(@country_code).panel_provider
+    unit_price = Pricing.new(panel_provider).price
+    # The target group id isn't used here and i think it should have a reason
+    # to be in post body target groups look like categories based on functionality
+    # so i think locations may have related to target group and the total size
+    # will be the sum of panels that in location group and target group
+    total_size = @locations.reduce(0) { |sum, val| val[:size] + sum }
+
+    unit_price * total_size
+  end
 end
--- project/spec/models/target_spec.rb
+++ project/spec/models/target_spec.rb
@@ -1,9 +1,53 @@
-require 'rspec'
+require 'rails_helper'
 
-describe 'My behaviour' do
+describe Target do
+  let!(:country) { create :country }
+  let!(:target_group) { create :target_group, panel_provider: country.panel_provider }
+  let!(:locations) { create_list :location, 5 }
+  context 'when params is valid' do
+    let(:valid_params) do
+      {
+        locations: locations.map { |loc| { id: loc.id, size: Faker::Number.number(3) } },
+        target_group_id: target_group.id,
+        country_code: country.code
+      }
+    end
+    before do
+      country.target_groups << target_group
+      country.save!
+    end
 
-  it 'should do something' do
+    it 'is valid and no error messages' do
+      target = Target.new(valid_params[:country_code],
+                          valid_params[:target_group_id],
+                          valid_params[:locations])
 
-    true.should == false
+      expect(target.valid?).to be_truthy
+    end
+  end
+
+  context 'when params is invalid' do
+    let(:invalid_params) do
+      {
+        locations: [{ id: -1, size: 1000 }],
+        target_group_id: -1,
+        country_code: -1,
+      }
+    end
+    subject do
+      Target.new(invalid_params[:country_code],
+                 invalid_params[:target_group_id],
+                 invalid_params[:locations])
+    end
+
+    it 'is not valid' do
+      expect(subject.valid?).to be_falsey
+    end
+
+    it 'give 3 errors messages' do
+      subject.valid?
+
+      expect(subject.errors.size).to eq(3)
+    end
   end
 end
